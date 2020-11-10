3da75fb2e1f39e1e73dee1317532ae33a04c6478
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-10 00:06:57 -0500

    modifying the filename

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -13,19 +13,17 @@ def call() {
                     returnStdout: true
                 ).trim()
         }
-
         stage('Build & Test') {
+                sh 'cd cicd'
                 sh 'mvn --version'
                 sh "mvn -Ddb_port=${config.DB_PORT} -Dredis_port=${config.REDIS_PORT} clean install"
         }
-
         stage ('Push Docker Image') {
             docker.withRegistry('https://navaneethreddydevops.com', 'dockerhub') {
                 sh "docker build -t navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER} ."
                 sh "docker push navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER}"
             }
         }
-
         stage ('Deploy') {
             echo "We are going to deploy ${p.SERVICE_NAME}"
             sh "kubectl set image deployment/${p.SERVICE_NAME} ${config.SERVICE_NAME}=opstree/${config.SERVICE_NAME}:${BUILD_NUMBER} "
