adb5b2f20a8f2dbc9d134bfc19b3280c70c89ed7
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-10 00:09:54 -0500

    modifying the filename

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -14,8 +14,8 @@ def call() {
                 ).trim()
         }
         stage('Build & Test') {
-                sh 'cd cicd'
                 sh 'mvn --version'
+                sh 'cd cicd'
                 sh 'ls -lrt'
                 sh "mvn -Ddb_port=${config.DB_PORT} -Dredis_port=${config.REDIS_PORT} clean install"
         }
