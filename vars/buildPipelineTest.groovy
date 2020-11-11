8ae0608ad238574697775403934707fbccae7262
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-10 21:49:27 -0500

    adding the pipeline stages

--- vars/buildPipelineTest.groovy
+++ vars/buildPipelineTest.groovy
@@ -35,7 +35,7 @@ def call() {
                     script {
                         checkout scm
                         config = pipelineConfig()
-                        println 'Properties from App Repo :' + config 'Pipeline params'+ params
+                        println 'Properties from App Repo :' + config + params
                     }
                 }
             }
