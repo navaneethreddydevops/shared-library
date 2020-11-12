35395ad76614e4bc575490c6b08285c7428b1bc7
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-12 18:25:08 -0500

    adding the mapping for environment

--- src/com/utils/pipelineConfig.groovy
+++ src/com/utils/pipelineConfig.groovy
@@ -1,4 +1,11 @@
-package com.util.config
+package com.utils
+
+//imports
+
+import com.cloudbees.groovy.cps.NonCPS
+import groovy.json.JsonOutput
+import hudson.FilePath
+import jenkins.model.Jenkins
 
 def call() {
     Map pipelineConfig = readYaml(file: "${WORKSPACE}/pipeline.yaml")
