8027b4f2bc4f9f978b541f92f20879fa64220a05
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-13 16:13:43 -0500

    adding branch based condition

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -8,6 +8,12 @@ def call() {
         agent {
             label ('master')
         }
+        triggers {
+            cron('H */4 * * 1-5')
+        }
+        tools {
+            maven 'apache-maven-3.0.1'
+        }
         options {
             parallelsAlwaysFailFast()
             timeout(time: 1, unit: 'HOURS')
@@ -17,6 +23,9 @@ def call() {
             choice (name: 'Environment', choices: ['MyCustomCloudDev', 'MyCustomCloudQA', 'MyCustomCloudProd'], description: 'Environment')
             choice (name: 'Region', choices: ['us-east-1', 'us-west-2', 'eu-west-2', 'ap-southeast-2'], description: 'RegionToDeploy')
             choice (name: 'app', choices: ['Java', 'Python', 'Terraform', 'Docker', 'Helm', 'Cloudformation', 'Lambda'], description: 'BuildType')
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
         }
         stages {
             stage('ParameterCheck') {
@@ -63,26 +72,43 @@ def call() {
                     }
                 }
             }
+            stage('Approval') {
+            input {
+                message "Should we continue?"
+                ok "Yes, we should."
+                submitter "alice,bob"
+                parameters {
+                    string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                }
+            }
+            stage('Example Deploy') {
+            when {
+                branch 'production'
+            }
+            steps {
+                echo 'Deploying'
+            }
+        }
 
-            stage ('Push Docker Image') {
-                steps {
-                    script {
-                        docker.withRegistry('https://navaneethreddydevops.com', 'dockerhub') {
-                            sh "docker build -t navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER} ."
-                            sh "docker push navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER}"
-                        }
-                    }
-                }
-            }
-            stage ('Deploy') {
-                steps {
-                    script {
-                        echo "We are going to deploy ${p.SERVICE_NAME}"
-                        sh "kubectl set image deployment/${p.SERVICE_NAME} ${config.SERVICE_NAME}=opstree/${config.SERVICE_NAME}:${BUILD_NUMBER} "
-                        sh "kubectl rollout status deployment/${config.SERVICE_NAME} -n ${config.ENVIRONMENT_NAME} "
-                    }
-                }
-            }
+            // stage ('Push Docker Image') {
+            //     steps {
+            //         script {
+            //             docker.withRegistry('https://navaneethreddydevops.com', 'dockerhub') {
+            //                 sh "docker build -t navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER} ."
+            //                 sh "docker push navaneethreddydevops.com/${config.SERVICE_NAME}:${BUILD_NUMBER}"
+            //             }
+            //         }
+            //     }
+            // }
+            // stage ('Deploy') {
+            //     steps {
+            //         script {
+            //             echo "We are going to deploy ${p.SERVICE_NAME}"
+            //             sh "kubectl set image deployment/${p.SERVICE_NAME} ${config.SERVICE_NAME}=opstree/${config.SERVICE_NAME}:${BUILD_NUMBER} "
+            //             sh "kubectl rollout status deployment/${config.SERVICE_NAME} -n ${config.ENVIRONMENT_NAME} "
+            //         }
+            //     }
+            // }
         }
     }
 }
