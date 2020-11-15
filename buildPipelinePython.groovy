adf6ccbe493bf1950016e7e03d586911071ca3bf
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-15 10:33:59 -0500

    adding the python pipeline

---
+++ buildPipelinePython.groovy
@@ -0,0 +1,111 @@
+#!/usr/bin/groovy
+/* groovylint-disable LineLength, NestedBlockDepth */
+
+def call() {
+    def config
+    currentBuild.result = 'SUCCESS'
+    pipeline {
+        agent {
+            label ('master')
+        }
+        triggers {
+            cron('H */4 * * 1-5')
+        }
+        // tools {
+        //     maven 'apache-maven-3.0.1'
+        // }
+        options {
+            parallelsAlwaysFailFast()
+            timeout(time: 1, unit: 'HOURS')
+            buildDiscarder (logRotator(numToKeepStr: '10'))
+        }
+        parameters {
+            choice (name: 'Environment', choices: ['MyCustomCloudDev', 'MyCustomCloudQA', 'MyCustomCloudProd'], description: 'Environment')
+            choice (name: 'Region', choices: ['us-east-1', 'us-west-2', 'eu-west-2', 'ap-southeast-2'], description: 'RegionToDeploy')
+            choice (name: 'app', choices: ['Java', 'Python', 'Terraform', 'Docker', 'Helm', 'Cloudformation', 'Lambda'], description: 'BuildType')
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+        }
+        stages {
+            stage('ParameterCheck') {
+                steps {
+                    script {
+                        if (
+                        (params.Environment == 'null' || params.Region == 'null')
+                        || (params.app == 'null' && params.app == '')
+                        ) {
+                            currentBuild.result = 'ABORTED'
+                            error('Prameters not provided properly')
+                        }
+                    }
+                }
+            }
+            stage('Checkout') {
+                steps {
+                    script {
+                        checkout scm
+                        config = pipelineConfig()
+                        println 'Properties from App Repo :' + config + params
+                    }
+                }
+            }
+        stage('Build environment') {
+            steps {
+                sh '''
+                python3 -m pip freeze > requirements.txt
+                pip3 install -r requirements.txt
+                '''
+            }
+        }
+        stage('Test environment') {
+            steps {
+                sh '''
+                pip3 list
+                which pip3
+                which python3
+                '''
+            }
+        }
+            stage('DockerCleanUp') {
+                steps {
+                    script {
+                        dockerClean()
+                    }
+                }
+            }
+            stage('Sonarqube') {
+                steps {
+                    script {
+                        withSonarQubeEnv('sonar') {
+                            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar ' +
+                            '-f pom.xml ' +
+                            '-Dsonar.projectKey=com.huettermann:all:master ' +
+                            '-Dsonar.language=java ' +
+                            '-Dsonar.sources=. ' +
+                            '-Dsonar.tests=. ' +
+                            '-Dsonar.test.inclusions=**/*Test*/** ' +
+                            '-Dsonar.exclusions=**/*Test*/**'
+                        }
+                    }
+                }
+            }
+            stage('SonarQube Quality Gate') {
+                steps {
+                    script {
+                        timeout(time: 5, unit: 'MINUTES') {
+                            def qualitygate = waitForQualityGate()
+                            if (qualitygate.status != 'OK') {
+                                abortPipeline:true
+                                error "Pipeline aborted due to quality gate failure:${qualitygate.status}"
+                            }
+                            else {
+                                echo 'Quality gate passed'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
