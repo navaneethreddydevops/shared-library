d8068a70fedd40779b1a104b9a492d29cb7e3ef5
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-15 10:01:45 -0500

    adding the code scan stage

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -69,7 +69,14 @@ def call() {
                 steps {
                     script {
                         withSonarQubeEnv('sonar') {
-                        println ${ env.SONAR_HOST_URL }
+                            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar ' +
+                            '-f all/pom.xml ' +
+                            '-Dsonar.projectKey=com.huettermann:all:master ' +
+                            '-Dsonar.language=java ' +
+                            '-Dsonar.sources=. ' +
+                            '-Dsonar.tests=. ' +
+                            '-Dsonar.test.inclusions=**/*Test*/** ' +
+                            '-Dsonar.exclusions=**/*Test*/**'
                         }
                     }
                 }
