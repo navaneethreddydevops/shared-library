680b0a6e98928e901127d9530c96ae974edf4b11
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2020-11-15 11:27:12 -0500

    adding unit test stage

--- vars/buildPipelinePython.groovy
+++ vars/buildPipelinePython.groovy
@@ -73,41 +73,54 @@ def call() {
                     }
                 }
             }
-            stage('Static code metrics') {
+            // stage('Static code metrics') {
+            // steps {
+            //     echo 'Raw metrics'
+            //     // sh  '''
+            //     //     radon raw --json irisvmpy > raw_report.json
+            //     //     radon cc --json irisvmpy > cc_report.json
+            //     //     radon mi --json irisvmpy > mi_report.json
+            //     //     sloccount --duplicates --wide irisvmpy > sloccount.sc
+            //     //     '''
+            //     echo 'Test coverage'
+            //     sh  '''
+            //         coverage run irisvmpy/iris.py 1 1 2 3
+            //         python3 -m coverage xml -o reports/coverage.xml
+            //         '''
+            //     echo 'Style check'
+            //     sh  '''
+            //         pylint irisvmpy || true
+            //         '''
+            // }
+            // post {
+            //     always {
+            //         step([$class: 'CoberturaPublisher',
+            //                        autoUpdateHealth: false,
+            //                        autoUpdateStability: false,
+            //                        coberturaReportFile: 'reports/coverage.xml',
+            //                        failNoReports: false,
+            //                        failUnhealthy: false,
+            //                        failUnstable: false,
+            //                        maxNumberOfBuilds: 10,
+            //                        onlyStable: false,
+            //                        sourceEncoding: 'ASCII',
+            //                        zoomCoverageChart: false])
+            //         }
+            //     }
+            // }
+            stage('Unit tests') {
             steps {
-                echo 'Raw metrics'
-                // sh  '''
-                //     radon raw --json irisvmpy > raw_report.json
-                //     radon cc --json irisvmpy > cc_report.json
-                //     radon mi --json irisvmpy > mi_report.json
-                //     sloccount --duplicates --wide irisvmpy > sloccount.sc
-                //     '''
-                echo 'Test coverage'
                 sh  '''
-                    coverage run irisvmpy/iris.py 1 1 2 3
-                    python3 -m coverage xml -o reports/coverage.xml
-                    '''
-                echo 'Style check'
-                sh  '''
-                    pylint irisvmpy || true
+                    python3 -m pytest --verbose --junit-xml reports/unit_tests.xml
                     '''
             }
             post {
                 always {
-                    step([$class: 'CoberturaPublisher',
-                                   autoUpdateHealth: false,
-                                   autoUpdateStability: false,
-                                   coberturaReportFile: 'reports/coverage.xml',
-                                   failNoReports: false,
-                                   failUnhealthy: false,
-                                   failUnstable: false,
-                                   maxNumberOfBuilds: 10,
-                                   onlyStable: false,
-                                   sourceEncoding: 'ASCII',
-                                   zoomCoverageChart: false])
-                    }
+                    // Archive unit tests for the future
+                    junit allowEmptyResults: true, testResults: 'reports/unit_tests.xml'
                 }
             }
+        }
             stage('SonarQube Quality Gate') {
                 steps {
                     script {
